1324f5b22833def7d6634a585b2c289a4477bac6
moncef <epiccoder00@gmail.com>
2023-10-03 22:04:03 +0100

    added light mode for the textaria

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "duration-100 rounded  w-full h-12 peer focus:shadow-inner dark:border  border-dark-700  focus:border-dark-300  bg-dark-950  p-2 focus:outline-none focus:ring-1  focus:ring-dark-400 placeholder:font-light placeholder:text-sm  placeholder:text-dark-500 focus:placeholder:text-dark-100 text-light-950 ",
+          "duration-100 rounded  w-full h-12 peer focus:shadow-inner border  dark:border-dark-700 border-light-700  dark:focus:border-dark-300 focus:border-light-300  dark:bg-dark-950 bg-light-50  p-2 focus:outline-none focus:ring-1  dark:focus:ring-dark-400 focus:ring-light-950 placeholder:font-light placeholder:text-sm  dark:placeholder:text-dark-500 placeholder:text-dark-300 dark:focus:placeholder:text-dark-50 dark:text-light-950 text-dark-900",
           className
         )}
         ref={ref}
