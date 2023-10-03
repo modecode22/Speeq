dfc90aea5b9e29293f0a16bf03c6c5c120358c2c
moncef <epiccoder00@gmail.com>
2023-10-03 22:01:43 +0100

    light mode for the password input

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -19,7 +19,7 @@ const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
         <div className="relative">
           <input
             className={cn(
-            "duration-100 rounded  w-full h-12 peer focus:shadow-inner border  dark:border-dark-700 border-light-700  dark:focus:border-dark-300 focus:border-light-300  dark:bg-dark-950 bg-light-50  p-2 focus:outline-none focus:ring-1  dark:focus:ring-dark-400 focus:ring-light-950 placeholder:font-light placeholder:text-sm  dark:placeholder:text-dark-500 placeholder:text-dark-300 dark:focus:placeholder:text-dark-50 dark:text-light-950 text-dark-900 ",
+              "duration-100 rounded  w-full h-12 peer focus:shadow-inner border  dark:border-dark-700 border-light-700  dark:focus:border-dark-300 focus:border-light-300  dark:bg-dark-950 bg-light-50  p-2 focus:outline-none focus:ring-1  dark:focus:ring-dark-400 focus:ring-light-950 placeholder:font-light placeholder:text-sm  dark:placeholder:text-dark-500 placeholder:text-dark-300 dark:focus:placeholder:text-dark-50 dark:text-light-950 text-dark-900 ",
               className,
               error &&
                 "border-error-600 focus:border-error-500 focus:ring-error-600"
