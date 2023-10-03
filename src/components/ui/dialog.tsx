beb6c0db167a583767b511d685ca87a530e2203c
moncef <epiccoder00@gmail.com>
2023-10-03 22:01:09 +0100

    added light node to the dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -23,7 +23,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 z-50 bg-dark-800/50 backdrop-blur-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 dark:bg-dark-800/50 backdrop-blur-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
@@ -40,13 +40,13 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid  w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4   bg-dark-700 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded md:w-full",
+        "fixed left-[50%] top-[50%] z-50 grid  w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4   dark:bg-dark-700 bg-light-50 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded md:w-full",
         className
       )}
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute text-light-50 right-4 top-4 rounded-sm opacity-70 ring-offset-primary-500 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-dark-500 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+      <DialogPrimitive.Close className="absolute dark:text-light-50 text-dark-500 right-4 top-4 rounded-sm opacity-70 ring-offset-primary-500 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-dark-500 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
         <RiCloseLine className="h-4 w-4 " />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
@@ -61,7 +61,7 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-light-50 text-center sm:text-left",
+      "flex flex-col space-y-1.5 dark:text-light-50 text-dark-500 text-center sm:text-left",
       className
     )}
     {...props}
@@ -104,7 +104,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-light-500", className)}
+    className={cn("text-sm  dark:text-light-500 text-dark-500", className)}
     {...props}
   />
 ));
