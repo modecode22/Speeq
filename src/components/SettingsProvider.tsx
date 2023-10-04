9a042bf494aee4e0e83d8700fda9bb6296b3e5c1
moncef <epiccoder00@gmail.com>
2023-10-04 21:39:17 +0100

    added the language settings

--- src/components/SettingsProvider.tsx
+++ src/components/SettingsProvider.tsx
@@ -5,11 +5,15 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 type ContextType = {
   theme: ThemeType;
   updateTheme: (theme: ThemeType) => void;
+  language: string;
+  updateLanguage: (language: string) => void;
 };
 // Language Context
 const SettingsContext = createContext<ContextType>({
   theme: "dark",
   updateTheme: () => {},
+  language:  "eng",
+  updateLanguage: () => {},
 });
 
 
@@ -19,10 +23,20 @@ export const SettingsProvider = ({ children }: { children :React.ReactNode}) => 
     JSON.parse(localStorage.getItem("theme") as ThemeType) ||
       "dark"
   ); // Default theme is dark
+  const [language, setLanguage] = useState<string>(
+    JSON.parse(localStorage.getItem("lang") as string) ||
+      "eng"
+  ); // Default theme is dark
 
     const updateTheme = (theme: ThemeType) => {
       setTheme(theme);
       localStorage.setItem("theme", JSON.stringify(theme));
+    };
+
+
+    const updateLanguage = (language: string) => {
+      setLanguage(language);
+      localStorage.setItem("lang", JSON.stringify(language));
     };
 
 
@@ -38,13 +52,18 @@ export const SettingsProvider = ({ children }: { children :React.ReactNode}) => 
         if (!localStorage.getItem("theme")) {
           localStorage.setItem("theme", JSON.stringify("dark"));
         }
+        if (!localStorage.getItem("lang")) {
+          localStorage.setItem("lang", JSON.stringify("eng"));
+        }
   }, []);
 
   return (
     <SettingsContext.Provider
       value={{
         theme,
-        updateTheme
+        updateTheme,
+        language,
+        updateLanguage
       }}
     >
       {children}
