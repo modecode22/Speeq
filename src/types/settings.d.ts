c4ee3c03d1c8116a680a24b1dc4db68fd926e966
moncef <epiccoder00@gmail.com>
2023-10-04 21:41:26 +0100

    added the language settings types

--- src/types/settings.d.ts
+++ src/types/settings.d.ts
@@ -4,3 +4,12 @@ type ThemeArray = [
     { option: "light"; name: "light" },
     { option: "dark"; name: "dark" }
   ];
+
+
+
+  type Language = {
+    shortName: string;
+    longName: string;
+  };
+  
+  type LanguageArray = Language[]
