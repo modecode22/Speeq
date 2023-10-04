aacd3da8c5713fabc0392b4b73a5ee3849d42458
moncef <epiccoder00@gmail.com>
2023-10-04 21:40:15 +0100

    started working with the real ui

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import UiPage from '../components/ui/ui-page'
-import ToggleThemeBtn from '../components/ToggleThemeBtn'
+import { RiSettings4Line, RiUser3Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import Recorder from "../components/Recorder";
 
-const Hone = () => {
+const Home = () => {
+  
   return (
-    <div>
-        <div className='p-5'>
-<ToggleThemeBtn/>
+    <section>
+      <header className="flex  items-center w-full justify-end p-3 gap-3">
+        <Link  to={'settings'}>
+          <RiUser3Line className="w-6 h-6" />
 
-        </div>
-        <UiPage/>
-    </div>
-  )
-}
+        </Link>
+      </header>
+      <main className="p-5 flex flex-col gap-5 select-none">
+        <section className="flex flex-col ">
+          <Recorder/>
+        </section>
+      </main>
+    </section>
+  );
+};
 
-export default Hone
+export default Home;
