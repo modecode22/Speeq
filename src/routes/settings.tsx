914dff4d9be8fd632440e7c18905ff2518e5734a
moncef <epiccoder00@gmail.com>
2023-10-05 22:20:59 +0100

    added animation for the header of the settings

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -2,14 +2,17 @@ import { RiArrowGoBackFill, RiUser3Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import ToggleThemeBtn from "../components/ToggleThemeBtn";
 import SelectLang from "../components/SelectLang";
-
+import { motion } from 'framer-motion'
+import { AnimatedButtonVariants } from "../lib/animation-utils";
 const Settings = () => {
   return (
     <section>
       <header className="flex  items-center w-full justify-end p-3 gap-3">
-        <Link to={"settings"}>
+            <motion.button initial={'initial'} exit={'exit'} whileHover={'hover'} animate={'animate'}  variants={AnimatedButtonVariants}>
+   <Link to={"settings"}>
           <RiArrowGoBackFill className="w-6 h-6" />
-        </Link>
+        </Link>       </motion.button>
+
       </header>
       <main className="p-5 flex flex-col gap-5 select-none">
         <section className="flex flex-col ">
