fc7ddb5ee188f4add75f3d434c7d90b017ecb1a2
moncef <epiccoder00@gmail.com>
2023-10-05 22:19:22 +0100

    the recorder

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,44 +1,18 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "./ui/button";
+import { motion } from "framer-motion";
+import { recordButtonVariants, svgVariants } from "../lib/animation-utils";
+import AudioPlayer from "./AudioPlayer";
 
 const Recorder: React.FC = () => {
-  const [recording, setRecording] = useState(false);
-  const [audioUrl, setAudioUrl] = useState<string | null>(null);  // <--- Add this line
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const audioChunksRef = useRef<Blob[]>([]);
-  const silenceDetectionCounterRef = useRef<number>(0);
 
   const startRecording = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    
-    const audioContext = new AudioContext();
-  const source = audioContext.createMediaStreamSource(stream);
-  const processor = audioContext.createScriptProcessor(512, 1, 1);
-  
-  source.connect(processor);
-  processor.connect(audioContext.destination);
-    processor.onaudioprocess = function(event) {
-      const inputData = event.inputBuffer.getChannelData(0);
-      const total = inputData.length;
-      let sum = 0;
-      
-      // Calculate the RMS (root mean square) to determine silence
-      for (let i = 0; i < total; i++) {
-        sum += inputData[i] * inputData[i];
-      }
-      const rms = Math.sqrt(sum / total);
-      
-      if (rms < 0.01) {  // Adjust threshold as needed
-        silenceDetectionCounterRef.current += 1;
-        if (silenceDetectionCounterRef.current > 20) {  // Adjust duration as needed
-          stopRecording();
-        }
-      } else {
-        silenceDetectionCounterRef.current = 0;
-      }
-    };
-
     mediaRecorderRef.current = new MediaRecorder(stream);
-    audioChunksRef.current = [];
 
     mediaRecorderRef.current.ondataavailable = (event) => {
       if (event.data.size > 0) {
@@ -47,40 +21,106 @@ const Recorder: React.FC = () => {
     };
 
     mediaRecorderRef.current.onstop = () => {
-      processor.disconnect();
-      audioContext.close();
-      const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/wav' });
-      const audioUrl = URL.createObjectURL(audioBlob);
-      const audio = new Audio(audioUrl);
-      audio.play();
+      const audioBlob = new Blob(audioChunksRef.current, { type: "audio/wav" });
+      const newAudioUrl = URL.createObjectURL(audioBlob);
+      setAudioUrl(newAudioUrl);
     };
 
     mediaRecorderRef.current.start();
-    setRecording(true);
+    setIsRecording(true);
   };
 
   const stopRecording = () => {
     if (mediaRecorderRef.current) {
       mediaRecorderRef.current.stop();
-      setRecording(false);
+      audioChunksRef.current = [];
+      setIsRecording(false);
     }
   };
 
 
+  
 
   return (
-    <div>
-      <button onClick={startRecording} disabled={recording}>
-        Start Recording
-      </button>
-      <button onClick={stopRecording} disabled={!recording}>
-        Stop Recording
-      </button>   {audioUrl && (
-        <div>
-          <audio controls src={audioUrl}></audio>
+    <section className="h-full w-full flex justify-center items-center flex-col gap-5">
+      <motion.button
+        initial="stop"
+        whileHover="hover"
+        animate={isRecording ? "record" : "stop"}        variants={recordButtonVariants}
+        className="flex flex-col items-center p-3 justify-evenly rounded-full"
+        onClick={() => {
+          if (isRecording) {
+            stopRecording();
+          } else {
+            startRecording();
+          }
+        }}
+      >
+        {isRecording ? (
+          <>
+            <motion.svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="3em"
+              width="3em"
+              xmlns="http://www.w3.org/2000/svg"
+              variants={svgVariants}
+              initial="initial"
+              whileHover="hover"
+
+            >
+              <motion.path
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                }}
+                strokeWidth={1}
+                fill="none"
+                initial={{ pathLength: 0 }}
+                animate={{ pathLength: 1 }}
+                d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM9 9V15H15V9H9Z"
+              />
+            </motion.svg>{" "}
+            Stop
+          </>
+        ) : (
+          <>
+            <motion.svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="3em"
+              width="3em"
+              xmlns="http://www.w3.org/2000/svg"
+              variants={svgVariants}
+              initial="initial"
+              whileHover="hover"
+            >
+              <motion.path
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                }}
+                strokeWidth={1}
+                fill="none"
+                initial={{ pathLength: 0 }}
+                animate={{ pathLength: 1 }}
+                d="M11.9998 1C14.7612 1 16.9998 3.23858 16.9998 6V10C16.9998 12.7614 14.7612 15 11.9998 15C9.23833 15 6.99976 12.7614 6.99976 10V6C6.99976 3.23858 9.23833 1 11.9998 1ZM3.05469 11H5.07065C5.55588 14.3923 8.47329 17 11.9998 17C15.5262 17 18.4436 14.3923 18.9289 11H20.9448C20.4837 15.1716 17.1714 18.4839 12.9998 18.9451V23H10.9998V18.9451C6.82814 18.4839 3.51584 15.1716 3.05469 11Z"
+              ></motion.path>
+            </motion.svg>{" "}
+            Record
+          </>
+        )}
+      </motion.button>
+      {audioUrl && (
+        <div className="flex flex-col items-center mt-2 w-full">
+       <AudioPlayer isRecording={isRecording} src={audioUrl}/>
         </div>
       )}
-    </div>
+    </section>
   );
 };
 
