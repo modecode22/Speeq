524fcc2ab3c1b96047c4bac03b15269e7ffb32f2
moncef <epiccoder00@gmail.com>
2023-10-09 21:07:03 +0100

    changed the layout style for the rroot

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -15,7 +15,7 @@ import CustomTitlebar from "../components/CustomTitlebar";
        <main className="relative w-full  dark:bg-dark-900 bg-light-100 text-dark-800  dark:text-light-50  h-screen max-h-screen flex flex-col  ">
               <CustomTitlebar />
 
-           <ScrollArea className="message-back w-full h-full mt-8 flex justify-center items-center flex-col gap-5">
+           <ScrollArea className="message-back w-full h-full mt-8 flex justify-center items-center flex-col gap-5 flex-grow ">
              <Outlet />
            </ScrollArea>
          <Toaster />
