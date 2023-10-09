7000928d9903ccecbdec6d3e0f1790bef3961b4d
moncef <epiccoder00@gmail.com>
2023-10-09 21:06:29 +0100

    added util function for local storage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+
+  // Helper function to get and parse localStorage data
+ export const getFromLocalStorage = (key: string, defaultValue: any) => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : defaultValue;
+  };
