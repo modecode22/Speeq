0b3bc97b7c444d2d286d2cea0c360a0f28f76d7d
moncef <epiccoder00@gmail.com>
2023-10-10 15:16:16 +0100

    added select form land component

---
+++ src/components/SelectFromLang.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import { useSettings } from './SettingsProvider';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { SourceLanguages } from '../lib/data';
+
+const SelectFromLang = () => {
+  const { fromLanguage, updateFromLanguage } = useSettings();
+
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+      setMounted(true);
+    }, []);
+  return (
+    <Select
+    onValueChange={(value: string) => {
+      updateFromLanguage(value);
+    }}
+    defaultValue={fromLanguage}
+  >
+    <SelectTrigger name="from language" className="w-full">
+      <SelectValue placeholder="Select language" />
+    </SelectTrigger>
+    <SelectContent
+      onSelect={(e) => {
+        e.target;
+      }}
+      className="max-h-[300px] "
+    >
+        {SourceLanguages.map((fromlang) => {
+          return (
+            <SelectItem key={fromlang} value={fromlang}>
+              {fromlang}
+            </SelectItem>
+          );
+        })}
+    </SelectContent>
+  </Select>  )
+}
+
+export default SelectFromLang
